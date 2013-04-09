3cbf5d82aba62e90f5dcfc5180f7fd2e9cb1e633
kareem1234 <kareem.lewis79@gmail.com>
2013-04-09 16:20:30 -0300

    Update app.js
    added commments

--- Webapp/app.js
+++ Webapp/app.js
@@ -3,17 +3,14 @@
  */
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 
-// database setup, host variable is local host ip
-// mongo db has to be installed
-// tutorial at http://www.youtube.com/watch?v=s8CaDhYq6Vw&list=PLw2e3dFxewkIjQ0Kr1LQ-nRlHrqTuPeA8&index=15
-
+// create app variable
 var app = express();
 
+// setup configuration 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -30,6 +27,8 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+
+// define rout paths
 app.get('/', routes.index);
 app.get('/home', routes.index);
 app.get('/topvideos', routes.topPlayed);
@@ -37,6 +36,8 @@ app.get('/search', routes.search);
 app.post('/search',routes.postSearch);
 app.get('/stream',routes.stream);
 
+
+// start server event loop
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
